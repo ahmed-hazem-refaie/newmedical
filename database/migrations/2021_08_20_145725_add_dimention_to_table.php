f7aff64eb85c583310be68e3684234f0ddfb64b8
ahmed-hazem-refaie <ahmed_hazem_refaie@hotmail.com>
2021-09-01 14:55:16 +0200

    second2 commit

---
+++ database/migrations/2021_08_20_145725_add_dimention_to_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDimentionToTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('setting_fields', function (Blueprint $table) {
+            $table->integer('dim1')->nullable();
+            $table->integer('dim2')->nullable();
+            $table->string('validation')->nullable();
+            $table->string('validation_msg')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('setting_fields', function (Blueprint $table) {
+            //
+        });
+    }
+}
