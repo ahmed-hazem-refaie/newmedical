1d82e68ce1f1bfa894cda3fb0942f75638b5ec1e
AhmedSaidShahata <ahmedsaidshehata1@gmail.com>
2021-09-09 22:25:54 +0200

    handle invitation

--- resources/views/includes/start.blade.php
+++ resources/views/includes/start.blade.php
@@ -3,40 +3,34 @@
         <div class="med">
 
             @php
-               $header_media =  field_content($settings,'Home Page' , 'header_media');
+            $header_media = field_content($settings,'Home Page' , 'header_media');
 
-               @endphp
-            
+            @endphp
+
             @if ($header_media == 'video')
-                <video muted autoplay loop width="320" height="240" controls>
-                    <source src="http://localhost:8000/storage/videos/oCOLuW2TBvJ6qPcJ19NqAaGWXBZwFvYaiIEuKnlX.mp4" type="video/mp4">
-                    <source src="movie.ogg" type="video/ogg">
+            <video muted autoplay loop width="320" height="240" controls>
+                <source src="http://localhost:8000/storage/videos/oCOLuW2TBvJ6qPcJ19NqAaGWXBZwFvYaiIEuKnlX.mp4" type="video/mp4">
+                <source src="movie.ogg" type="video/ogg">
                 Your browser does not support the video tag.
-                </video>
-            
+            </video>
+
 
             @else
             <img src="./assets/images/header1.png" alt="image">
             @endif
 
-            </div>
+        </div>
 
-      
-            
+
+
         <div class="container">
-            <div class="row flex-row-reverse">
+            <div class="row flex-row-reverse ">
                 <div class="over-layer"></div>
-                <div class="col-md-5   fadeInUp" data-wow-duration="3s" data-wow-delay="1s">
-                 
-                    
-          
-                    
-                    <a href="#" class="read next3">
-                        <span>Read More</span>
+                <a href="#" class="read next3">
+                    <span>Read More</span>
 
-                        <div class="bg-btn3"></div>
-                    </a>
-                </div>
+                    <div class="bg-btn3"></div>
+                </a>
             </div>
         </div>
     </div>
