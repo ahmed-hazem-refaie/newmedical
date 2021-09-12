fca0a7ce76dcefb92de9df140a75d63cfa535593
AhmedSaidShahata <ahmedsaidshehata1@gmail.com>
2021-09-12 22:19:37 +0200

    handle invitation

--- resources/views/includes/start.blade.php
+++ resources/views/includes/start.blade.php
@@ -12,14 +12,7 @@
 
             Your browser does not support the video tag.
     </video>
-    <a href="#" class="read next3" style="z-index: 99;   position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%,-50%);
-    padding: 6px 18px;     background-color: #860a0e;">
-        <span>Read More</span>
-        <div class="bg-btn3"></div>
-    </a>
+   
     @endif
 
     <div class="contain por " style="height: 70vh;">
@@ -32,10 +25,6 @@
 
             <img src="{{field_content($settings , 'Home Page' , 'header-image') ?? '/assets/images/header1.png' }}" alt="image">
 
-            <a href="#" class="read next3" style="z-index: 99;   top: 46%;">
-                <span>Read More</span>
-                <div class="bg-btn3"></div>
-            </a>
             @endif
 
         </div>
