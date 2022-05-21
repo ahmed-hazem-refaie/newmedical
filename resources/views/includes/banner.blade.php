de967d8dc8cf1696079de40134b2b5fbd5a69c7b
unknown <asaid@hulultech.com>
2022-05-21 23:55:30 +0200

    handle localization

--- resources/views/includes/banner.blade.php
+++ resources/views/includes/banner.blade.php
@@ -1,12 +1,20 @@
-<div class="banner-area fix area-padding">
+<div class="banner-area fix area-padding" style="background-image: url( {{field_content($settings ,'Home page' , 'section-banner-image') ?? '/assets/img/background/bg2.jpg'}})">
         <div class="container">
             <div class="row">
                 <div class="col-md-12 col-sm-12 col-xs-12">
                     <div class="add-content">
-                        <h3>Render It solutions corporate company established since 1998</h3>
+                        <h3>
+                        {{field_content($settings,'Home Page', 'section-banner-title') ?? "Render It solutions corporate company established since 1998"}}
+                    </h3>
                         <div class="add-contact">
-                            <span class="call-us"><i class="ti-mobile"></i>Toll free : +4321-7654543</span>
-                            <span class="call-us mail-us"><i class="ti-email"></i>Mail us : render-09@mail.com</span>
+                            <span class="call-us"><i class="ti-mobile"></i>
+                            {{field_content($settings,'Home Page', 'section-banner-phone') ?? " Tell free :+4321-7654543"}}
+  
+                        
+                        </span>
+                            <span class="call-us mail-us"><i class="ti-email"></i>
+                            {{field_content($settings,'Home Page', 'section-banner-phone') ?? "Mail us :  render-09@mail.com"}}
+                          </span>
                         </div>
                     </div>
                 </div>
--- resources/views/includes/counter.blade.php
+++ resources/views/includes/counter.blade.php
@@ -1,53 +1,73 @@
 <div class="counter-area fix area-padding">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 col-sm-6 col-xs-12">
-                    <div class="counter-image">
-                        <div class="rotmate-image rotateme">
-                            <img src="{{asset('assets/img/about/circle.png')}}" alt="">
-                        </div>
-                        <div class="top-img">
-                            <img src="  {{asset('assets/img/about/self.png')}}" alt="">
-                        </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <div class="counter-image">
+                    <div class="rotmate-image rotateme">
+                        <img src="{{asset('assets/img/about/circle.png')}}" alt="">
+                    </div>
+                    <div class="top-img">
+                        <img src="{{field_content($settings ,'Home page' , 'section-counter-image') ?? '/assets/img/about/self.png'}}" alt="">
                     </div>
                 </div>
-                <div class="col-md-6 col-sm-6 col-xs-12">
-                    <div class="fun-text-all">
-                        <!-- single counter  -->
-                        <div class="col-md-6 col-sm-6 col-xs-12">
-                            <div class="fun_text wow fadeInUp" data-wow-delay="0.2s">
-                                <span class="counter-icon"><i class="ti-layers"></i></span>
-                                <span class="counter">510</span>
-                                <h4>Total projects</h4>
-                            </div>
+            </div>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <div class="fun-text-all">
+                    <!-- single counter  -->
+                    <div class="col-md-6 col-sm-6 col-xs-12">
+                        <div class="fun_text wow fadeInUp" data-wow-delay="0.2s">
+                            <span class="counter-icon"><i class="ti-layers"></i></span>
+                            <span class="counter">
+                                {{field_content($settings,'Home Page', 'section-banner-project-count') ?? "510"}}
+                            </span>
+                            <h4>
+                                {{field_content($settings,'Home Page', 'section-banner-project-title') ?? "Total projects"}}
+                            </h4>
                         </div>
-                        <!-- single counter  -->
-                        <div class="col-md-6 col-sm-6 col-xs-12">
-                            <div class="fun_text wow fadeInUp" data-wow-delay="0.3s">
-                                <span class="counter-icon"><i class="ti-world"></i></span>
-                                <span class="counter">120</span>
-                                <h4>Worldwide</h4>
-                            </div>
-                        </div>
-                        <!-- single counter  -->
-                        <div class="col-md-6 col-sm-6 col-xs-12">
-                            <div class="fun_text wow fadeInUp" data-wow-delay="0.4s">
-                                <span class="counter-icon"><i class="ti-cup"></i></span>
-                                <span class="counter">40</span>
-                                <h4>Awards win</h4>
-                            </div>
-                        </div>
-                        <!-- single counter  -->
-                        <div class="col-md-6 col-sm-6 col-xs-12">
-                            <div class="fun_text wow fadeInUp" data-wow-delay="0.5s">
-                                <span class="counter-icon"><i class="ti-user"></i></span>
-                                <span class="counter">210</span>
-                                <h4>Team member</h4>
-                            </div>
-                        </div>
-                        <!-- single counter  -->
                     </div>
+                    <!-- single counter  -->
+                    <div class="col-md-6 col-sm-6 col-xs-12">
+                        <div class="fun_text wow fadeInUp" data-wow-delay="0.3s">
+                            <span class="counter-icon"><i class="ti-world"></i></span>
+
+                            <span class="counter">
+                                {{field_content($settings,'Home Page', 'section-banner-world-count') ?? "120"}}
+                            </span>
+                            <h4>
+                                {{field_content($settings,'Home Page', 'section-banner-world-title') ?? "Worldwide"}}
+                            </h4>
+
+                        </div>
+                    </div>
+                    <!-- single counter  -->
+                    <div class="col-md-6 col-sm-6 col-xs-12">
+                        <div class="fun_text wow fadeInUp" data-wow-delay="0.4s">
+                            <span class="counter-icon"><i class="ti-cup"></i></span>
+
+                            <span class="counter">
+                                {{field_content($settings,'Home Page', 'section-banner-awards-count') ?? "40"}}
+                            </span>
+                            <h4>
+                                {{field_content($settings,'Home Page', 'section-banner-awards-title') ?? "Awards win"}}
+                            </h4>
+
+                        </div>
+                    </div>
+                    <!-- single counter  -->
+                    <div class="col-md-6 col-sm-6 col-xs-12">
+                        <div class="fun_text wow fadeInUp" data-wow-delay="0.5s">
+                            <span class="counter-icon"><i class="ti-user"></i></span>
+                            <span class="counter">
+                                {{field_content($settings,'Home Page', 'section-banner-Team-count') ?? "210"}}
+                            </span>
+                            <h4>
+                                {{field_content($settings,'Home Page', 'section-banner-Team-title') ?? "Team member"}}
+                            </h4>
+                        </div>
+                    </div>
+                    <!-- single counter  -->
                 </div>
             </div>
         </div>
     </div>
+</div>
