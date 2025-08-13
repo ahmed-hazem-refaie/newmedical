38ab50326f2cec186039b703fa4d2bea42abe763
a.hazem <a.hazem@hulultech.com>
2025-08-13 09:24:33 +0300

    medi

--- app/Http/Controllers/Admin/ServiceController.php
+++ app/Http/Controllers/Admin/ServiceController.php
@@ -43,11 +43,11 @@ class ServiceController extends Controller
         $this->validate($request , [
 
 
-        'image_ar'=>'required|array',
+        'image_ar'=>'required|image',
         // 'image_ar.*'=>'required|image|dimensions:min_width=1900,min_height=600,max_width=2080,max_height=1280',
 
 
-        'image_en'=>'required|array',
+        'image_en'=>'required|image',
         // 'image_en.*'=>'required|image|dimensions:min_width=1900,min_height=600,max_width=2080,max_height=1280',
 
 
